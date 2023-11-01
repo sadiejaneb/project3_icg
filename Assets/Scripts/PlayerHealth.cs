f96bcf87cf5df3a5d3f62e9168c4f1a8ac623a96
sadiejaneb <sadiejaneb@gmail.com>
2023-11-01 16:35:30 -0400

    imported combat sounds and music

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -5,14 +5,29 @@ public class PlayerHealth : MonoBehaviour
     public int lives = 3;  // Player's total lives
     public delegate void PlayerDied();  // Delegate for player death event
     public static event PlayerDied OnPlayerDied;  // Event triggered when the player dies
-   
 
-   
+    public AudioClip damageSound;  // Drag the damage sound clip here in the inspector
+    private AudioSource audioSource;  // Reference to the AudioSource component
+
+    private void Start()
+    {
+        // Get the AudioSource component
+        audioSource = GetComponent<AudioSource>();
+        // Ensure that the audio source exists
+        if (audioSource == null)
+        {
+            Debug.LogWarning("No AudioSource found on the player. Adding one.");
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
 
     // This method gets called when the player is hit by a bullet
     public void TakeDamage(int damageAmount)
     {
         lives -= damageAmount;
+
+        // Play the damage sound
+        PlayDamageSound();
 
         if (lives <= 0)
         {
@@ -31,5 +46,12 @@ public class PlayerHealth : MonoBehaviour
 
         Debug.Log("Player Died!");
     }
-  
+
+    void PlayDamageSound()
+    {
+        if (damageSound && audioSource)
+        {
+            audioSource.PlayOneShot(damageSound);
+        }
+    }
 }
